d5bfb266983e4bd1e38cacf50fac74c8485c92d6
sampurkiss <samuelpurkiss@gmail.com>
2019-10-04 15:19:35 -0400

    updates

---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Sam Purkiss'
+SITENAME = "Sam's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Toronto'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
+
+# if you create jupyter files in the content dir, snapshots are saved with the same
+# metadata. These need to be ignored. 
+IGNORE_FILES = [".ipynb_checkpoints"]  
+
+
+# Change theme
+THEME = 'C:/Users/sam purkiss/Documents/Code/Themes/pelican-themes/blue-penguin'
+# Social widget
+MENUITEMS = (
+    ('GitHub', 'https://github.com/sampurkiss'),
+    ('LinkedIn', 'https://www.linkedin.com/in/samuelpurkiss/'),
+)
